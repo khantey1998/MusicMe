d50ba8c655f2340a52c5273ea5a592fccb856ba4
khantey1998 <khanteylong@gmail.com>
2018-06-10 11:29:21 +0700

    i added media player

--- app/src/main/java/com/fe/musicme/PlayMusicActivity.java
+++ app/src/main/java/com/fe/musicme/PlayMusicActivity.java
@@ -1,9 +1,12 @@
 package com.fe.musicme;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class PlayMusicActivity extends AppCompatActivity {
+
+    MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
